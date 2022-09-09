d4f02a56c75482789e88d111d633adf5e14a451e
Jude Gathoga <judegathoga@gmail.com>
2022-09-09 20:31:33 +0000

    Complete task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+ 
+ /**
+  * main - Prints the alphabet in lowercase, then uppercase
+  * followed by a new line.
+  *
+  * Return: 0 if the program exits successfully
+  */
+ int main(void)
+  {
+          char alph = 97;
+ 
+          while (alph <= 122)
+                  putchar(alph++);
+
+	  alph = 65;
+
+	  while (alph <= 90)
+		  putchar(alph++);
+
+          putchar('\n');
+          return (0);
+ }
