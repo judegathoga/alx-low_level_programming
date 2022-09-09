18514f621d02ce15328038937a74c503afb58b4e
Jude Gathoga <judegathoga@gmail.com>
2022-09-09 16:01:50 +0000

    Complete task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+*
+* Author: Jude G.
+* File: 5-printf.c
+*
+* Description: Prints exactly
+* with proper grammar, but the outcome is a piece of art,
+*/
+
+#include <stdio.h>
+
+/**
+* main - Function the prints a single line and returns 0
+* Return: 0 if the program exits succesfully
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
