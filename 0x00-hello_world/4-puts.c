e92b809c307bfa28e316929fcd672ec8f98c2497
Jude Gathoga <judegathoga@gmail.com>
2022-09-09 15:34:09 +0000

    Complete task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ *
+ * Author: Jude G.
+ * File: 4-puts.c
+ *
+ * Description: Prints exactly 
+ * "Programming is like building a multilingual puzzle
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
