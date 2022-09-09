af3e7d21bf76d22404530bb9cfb920b199c01659
Jude Gathoga <judegathoga@gmail.com>
2022-09-09 20:35:21 +0000

    Correct task 3 to betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,21 @@
  #include <stdio.h>
- 
- /**
-  * main - Prints the alphabet in lowercase, then uppercase
-  * followed by a new line.
-  *
-  * Return: 0 if the program exits successfully
-  */
- int main(void)
-  {
-          char alph = 97;
- 
-          while (alph <= 122)
-                  putchar(alph++);
+/**
+ * main - Prints the alphabet in lowercase, then uppercase
+ * followed by a new line.
+ *
+ * Return: 0 if the program exits successfully
+ */
+int main(void)
+{
+	char alph = 97;
 
-	  alph = 65;
+	while (alph <= 122)
+		putchar(alph++);
 
-	  while (alph <= 90)
-		  putchar(alph++);
+	alph = 65;
 
-          putchar('\n');
-          return (0);
- }
+	while (alph <= 90)
+		putchar(alph++);
+	putchar('\n');
+	return (0);
+}
