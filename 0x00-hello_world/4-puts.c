b837cc22d66c742e289f5cad207584b0c2367e91
Jude Gathoga <judegathoga@gmail.com>
2022-09-09 15:55:11 +0000

    Correct task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,20 @@
  * Author: Jude G.
  * File: 4-puts.c
  *
- * Description: Prints exactly 
+ * Description: Prints exactly
  * "Programming is like building a multilingual puzzle
  */
 
 #include <stdio.h>
+
+/**
+ * main - Function the prints a single line and returns 0
+ * Return: 0 if the prigram exits succesfully
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return 0;
+	return (0);
 }
