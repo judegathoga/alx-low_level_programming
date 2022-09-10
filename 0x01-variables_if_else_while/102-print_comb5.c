2717eb523c2411b0b6071256432d879de7e41ddd
Jude Gathoga <judegathoga@gmail.com>
2022-09-10 12:37:08 +0000

    Correct task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,40 +3,30 @@
 /**
  * main - Prints all possible combinations of two two-digit numbers.
  *
- * Return: 0 if the program exits successfully
+ * Return: 0 if the program exits successfully.
  */
 int main(void)
 {
-	int first, second, third, fourth, set_1, set_2;
+	int first, second;
 
-	for (first = 48; first < 58; first++)
+	for (first = 0; first = 98; first++)
 	{
-		for (second = 48; second < 58; second++)
+		for (second = first + 1; second <= 99; second++)
 		{
-			for (third = 48; third < 58; third++)
-			{
-				for (fourth = 48; fourth < 58; fourth++)
-				{
-					set_1 = 10 * (first + '0') + (second + '0');
-					set_2 = 10 * (third + '0') + (fourth + '0');
+			putchar((first / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((second / 10) + '0');
+			putchar((second % 10) + '0');
 
-					if (set_1 < set_2)
-					{
-						if (!(first == 48 && second == 48 && third == 48 && fourth == 49))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-						putchar(first);
-						putchar(second);
-						putchar(' ');
-						putchar(third);
-						putchar(fourth);
-					}
-				}
-			}
+			if (first == 98 && second == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
