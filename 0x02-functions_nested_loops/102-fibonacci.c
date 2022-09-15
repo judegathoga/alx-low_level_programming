a2572401db906cf35b324b5ac76b22cf4c0d0716
Jude Gathoga <judegathoga@gmail.com>
2022-09-15 18:48:26 +0000

    Complete task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	long unsigned int i, curr = 1, next = 2;
+
+	for (i = 1; i <= 25; i++)
+	{
+		printf("%lu, %lu", curr, next);
+		curr = curr +next;
+		next = curr + next;
+
+		if (i != 25)
+			printf(", ");
+	}
+
+	putchar('\n');
+	return (0);
+}
