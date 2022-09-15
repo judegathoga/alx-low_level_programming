be3768c2992f8dddc16daa349e18cff2d45d675a
Jude Gathoga <judegathoga@gmail.com>
2022-09-15 13:47:42 +0000

    Complete task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result.
+ * @one: First operand
+ * @two: Second operand
+ *
+ * Return: The sum of one and two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char);
 int print_last_digit(int);
 void times_table(void);
+int add(int, int);
