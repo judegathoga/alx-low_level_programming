f3a87ae3830aa12dae9023e0a16f453ec32ef12e
Jude Gathoga <judegathoga@gmail.com>
2022-09-15 19:09:44 +0000

    Complete task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program that finds and prints the sum of the even-valued terms,
+ * not greater than 4000000,followed by a new line.
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	unsigned curr = 1, next = 2;
+	unsigned long sum;
+
+	while (next <= 4000000)
+	{
+		if (curr % 2 ==0)
+			sum += curr;
+		if (next % 2 == 0)
+			sum += next;
+
+		curr = curr + next;
+		next = curr + next;
+	}
+
+	printf("%lu\n", sum);
+	return (0);
+}
